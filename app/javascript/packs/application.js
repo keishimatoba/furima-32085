ec2b50a92ddb1b4e7e631ef15c1c0493cf530028
keishimatoba <73728132+keishimatoba@users.noreply.github.com>
2020-11-25 19:20:18 +0900

    環境設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()　デフォ設定　これがあるとJavascriptが使えない
 require("@rails/activestorage").start()
 require("channels")
 
