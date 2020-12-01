879b8992d5acab3878c90eb4932f3ad5856c55eb
keishimatoba <73728132+keishimatoba@users.noreply.github.com>
2020-12-01 15:48:07 +0900

    userモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,:email,:birthday, presence: true
-         validates :family_name, presence: true
-         validates :first_name, presence: true
-         validates :family_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :email, uniqueness: true,
-                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-        #  validates :encrypted_password, confirmation: true
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX
-         validates :first_name,
-                   :family_name,
-                   :first_name_kana,
-                   :family_name_kana,
-                   format: {with: /[^ -~｡-ﾟ]+/}
+  validates :nickname, :email, :birthday, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :first_name,
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'First name Full-width katakana characters' }
+  validates :family_name,
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Family name Full-width katakana characters' }
+  validates :first_name_kana,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana Full-width katakana characters' }
+  validates :family_name_kana,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'Family name kana Full-width katakana characters' }
 
   has_many :items
   has_many :orders
-
-
 end
-
-                                
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+RSpec.describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it 'ユーザーの登録記載要素が全て揃えば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordが6文字以上であれば登録できること ' do
+        @user.password = 'a00000'
+        @user.password_confirmation = 'a00000'
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録がうまくいかないとき' do
+      it 'nicknameが空では登録できないこと' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'first_nameが空では登録できないこと' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it 'family_nameが空では登録できないこと' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+
+      it 'birthdayが空では登録できないこと' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+      it 'family_name_kanaが空では登録できないこと' do
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+      end
+
+      it 'first_name_kanaが空では登録できないこと' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+
+      it 'passwordが空では登録できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'passwordが5文字以下であれば登録できないこと' do
+        @user.password = 'a0000'
+        @user.password_confirmation = 'a0000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'family_nameが全角でないと登録できないこと' do
+        @user.family_name = 'abc'
+        @user.valid?
+        expect(@user.errors[:family_name]).to include('Family name Full-width katakana characters')
+      end
+
+      it 'first_nameが全角でないと登録できないこと' do
+        @user.first_name = 'abc'
+        @user.valid?
+        expect(@user.errors[:first_name]).to include('First name Full-width katakana characters')
+      end
+
+      it 'family_name_kanaが全角カタカナでないと登録できないこと' do
+        @user.family_name_kana = 'あいう'
+        @user.valid?
+        expect(@user.errors[:family_name_kana]).to include('Family name kana Full-width katakana characters')
+      end
+
+      it 'first_name_kanaが全角カタカナでないと登録できないこと' do
+        @user.first_name_kana = 'かきく'
+        @user.valid?
+        expect(@user.errors[:first_name_kana]).to include('First name kana Full-width katakana characters')
+      end
+    end
+  end
+end
