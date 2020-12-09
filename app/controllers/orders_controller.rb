819ab4b94f9435b551cf3aff30b00a3d89835eff
keishimatoba <73728132+keishimatoba@users.noreply.github.com>
2020-12-09 09:52:04 +0900

    PAY.JPを用いたクレジットカードでの決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase = Purchase.new(order_params)
       if @purchase.valid?
+         pay_item
          @purchase.save
          redirect_to root_path
       else
@@ -18,9 +19,19 @@ class OrdersController < ApplicationController
 
   end
 
-  private
+    private
 
     def order_params
-      params.require(:purchase).permit(:post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
+      params.require(:purchase).permit(:post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: order_params[:token],
+      currency: 'jpy'              
+    )
+    end
+
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,10 +8,11 @@ class Purchase
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/}
+    validates :token
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    address = Address.create(post_code: post_code, prefecture_code_id: prefecture_code_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order_id)
+    adress = Adress.create(post_code: post_code, prefecture_code_id: prefecture_code_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
